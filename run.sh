190abcf26a16a2e2357723abb662b28927b631ac
puchinya <puchinya@daradara.net>
2017-03-25 20:54:15 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -16,8 +16,7 @@ MYSQL_PASS=${MYSQL_PASS:-${MYSQL_ENV_MYSQL_PASS}}
 [ -z "${MYSQL_USER}" ] && { echo "=> MYSQL_USER cannot be empty" && exit 1; }
 [ -z "${MYSQL_PASS}" ] && { echo "=> MYSQL_PASS cannot be empty" && exit 1; }
 
-BACKUP_CMD="mysqldump -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASS} ${EXTRA_OPTS} ${MYSQL_DB} > /backup/"'${BACKUP_NAME}'
-
+BACKUP_CMD="mysqldump --max_allowed_packet=1G -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASS} ${EXTRA_OPTS} ${MYSQL_DB} | gzip > /backup/"'${BACKUP_NAME}'.gz
 echo "=> Creating backup script"
 rm -f /backup.sh
 cat <<EOF >> /backup.sh
